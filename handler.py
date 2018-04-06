224938df9d1716ac95035e91ef00f39e105ad416
baggio4ever <baggio4ever@gmail.com>
2018-04-07 08:54:23 +0900

    最高気温を取得してS3に保存できるようになった

--- handler.py
+++ handler.py
@@ -1,16 +1,68 @@
+# coding: UTF-8
 import json
+import urllib.request
+import csv
+# from bs4 import BeautifulSoup
+from datetime import date
+import os
+import logging
+import datetime
+import base64
+import codecs
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ 
+s3 = boto3.client('s3')
+BUCKET_NAME = os.environ['bucketName']
+
+# http://minus9d.hatenablog.com/entry/2015/07/07/225304
+
+# 気象庁
+# http://www.data.jma.go.jp/obd/stats/data/mdrr/docs/csv_dl_readme.html
 
 
 def hello(event, context):
+    # アクセスするURL
+    # 最高気温
+    url_highest = "http://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv"
+
+    # 最低気温
+    url_lowest = "http://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mntemsadext00_rct.csv"
+
+    # URLにアクセスする
+    now = date.today()
+    fn = now.strftime('%Y%m%d') + '.csv'
+    fn_utf8 = now.strftime('%Y%m%d') + '_utf8.csv'
+    full_fn = '/tmp/' + fn
+    urllib.request.urlretrieve(url_highest,full_fn)
+#    html = urllib.request.urlopen(url)
+
+    s3.upload_file(full_fn, BUCKET_NAME, fn)
+
+    utf8_full_fn = convert_sjis_to_utf8(full_fn)
+
+    s3.upload_file(utf8_full_fn, BUCKET_NAME, fn_utf8)
+
+    os.remove(full_fn)
+    os.remove(utf8_full_fn)
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
+        "filename": fn,
         "input": event
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "headers": {
+            "Access-Control-Allow-Origin":"*"
+        }
     }
+    
+    logger.info(response)
 
     return response
 
@@ -22,3 +74,21 @@ def hello(event, context):
         "event": event
     }
     """
+
+
+def convert_sjis_to_utf8(fn):
+    # Shift_JIS ファイルのパス
+    shiftjis_csv_path = fn
+    # UTF-8 ファイルのパス
+    ftitle, fext = os.path.splitext(fn)
+#    os.rename(f, ftitle + '_img' + fext)    
+    utf8_csv_path = ftitle + '_utf8' + fext
+
+    # 文字コードを utf-8 に変換して保存
+    fin = codecs.open(shiftjis_csv_path, "r", "shift_jis")
+    fout_utf = codecs.open(utf8_csv_path, "w", "utf-8")
+    for row in fin:
+        fout_utf.write(row)
+    fin.close()
+    fout_utf.close()
+    return utf8_csv_path
