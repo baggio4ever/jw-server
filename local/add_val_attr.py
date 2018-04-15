1bc525a2c2c8e89e0071ff9fc0a6f338c6fb9927
baggio4ever <baggio4ever@gmail.com>
2018-04-16 00:02:34 +0900

    rain24h 書き換えコード修正

--- local/add_val_attr.py
+++ local/add_val_attr.py
@@ -5,11 +5,20 @@ import boto3
 from time import sleep
 from boto3.dynamodb.conditions import Key, Attr
 import json
+import decimal
 
 # 文字列として登録されている 気温や降水量、積雪量を数値属性で追加する
 
 
 dynamo = boto3.resource('dynamodb',region_name='ap-southeast-2')
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
 
 def append_rainfall_amount_val(year,month,day):
 	TABLE_NAME = 'dev-jw-rain24h'
@@ -24,13 +33,36 @@ def append_rainfall_amount_val(year,month,day):
 	    KeyConditionExpression=Key('date').eq(d)
 	)
 
+	c0 = 0
+	c1 = 0
 	if response.get('Items'):
 		for i in response['Items']:
-		    print(i['date'], ":", i['rainfall_amount'])
+			if i.get('prefecture'):
+				i.pop('prefecture')
+			if i.get('place_no'):
+				i.pop('place_no')
+			if i.get('international_place_no'):
+				i.pop('international_place_no')
+			print(i['date'], ":", i['rainfall_amount'])
+			if i['rainfall_amount']=='-':
+				i['rainfall_amount_val'] = decimal.Decimal("-999.9")
+				c0 += 1
+			else:
+				i['rainfall_amount_val'] = decimal.Decimal(i['rainfall_amount'])
+				c1 += 1
 		
 		print('len(response["Items"] : {}'.format(len(response['Items'])))
+		print( 'c0: {}, c1: {}'.format(c0,c1) )
+
+		print('response :' + json.dumps(response, cls=DecimalEncoder))
+
+		with table.batch_writer() as batch:
+			for i in response['Items']:
+				batch.put_item(
+						Item=i
+				)
+
 	else:
 		print('あれれ。Itemsないみたい')
-		print('response :' + json.dumps(response))
 
 append_rainfall_amount_val(2018,4,8)
