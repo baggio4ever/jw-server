3c0c6915e9a1f7163c538272fbf7a92097dda36f
baggio4ever <baggio4ever@gmail.com>
2018-04-17 09:28:56 +0900

    数値化計画実行中

--- local/add_val_attr.py
+++ local/add_val_attr.py
@@ -20,6 +20,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
+# 降水量
 def append_rainfall_amount_val(year,month,day):
 	TABLE_NAME = 'dev-jw-rain24h'
 	table = dynamo.Table(TABLE_NAME)
@@ -66,6 +67,7 @@ def append_rainfall_amount_val(year,month,day):
 		print('あれれ。Itemsないみたい')
 
 
+# 最高気温
 def append_highest_temperature_val(year,month,day):
 	TABLE_NAME = 'dev-jw-highest'
 	table = dynamo.Table(TABLE_NAME)
@@ -113,6 +115,103 @@ def append_highest_temperature_val(year,month,day):
 		print('あれれ。Itemsないみたい')
 
 
+# 最低気温
+def append_lowest_temperature_val(year,month,day):
+	TABLE_NAME = 'dev-jw-lowest'
+	table = dynamo.Table(TABLE_NAME)
+	d = "{:04}/{:02}/{:02}".format(year,month,day)
+
+	print('---')
+	print('d : '+d)
+
+	response = table.query(
+		IndexName='globalIndex1',
+	    KeyConditionExpression=Key('date').eq(d)
+	)
+
+	c0 = 0
+	c1 = 0
+	if response.get('Items'):
+		for i in response['Items']:
+			if i.get('prefecture'):
+				i.pop('prefecture')
+			if i.get('place_no'):
+				i.pop('place_no')
+			if i.get('international_place_no'):
+				i.pop('international_place_no')
+			
+			print(i['date'], ":", i['temperature'])
+			if i['temperature']=='-':
+				i['temperature_val'] = decimal.Decimal("999.9")
+				c0 += 1
+			else:
+				i['temperature_val'] = decimal.Decimal(i['temperature'])
+				c1 += 1
+		
+		print('len(response["Items"] : {}'.format(len(response['Items'])))
+		print( 'c0: {}, c1: {}'.format(c0,c1) )
+
+		print('response :' + json.dumps(response, cls=DecimalEncoder))
+
+		with table.batch_writer() as batch:
+			for i in response['Items']:
+				batch.put_item(
+						Item=i
+				)
+
+	else:
+		print('あれれ。Itemsないみたい')
+
+
+# 積雪量
+def append_snow_depth_val(year,month,day):
+	TABLE_NAME = 'dev-jw-snow'
+	table = dynamo.Table(TABLE_NAME)
+	d = "{:04}/{:02}/{:02}".format(year,month,day)
+
+	print('---')
+	print('d : '+d)
+
+	response = table.query(
+		IndexName='globalIndex1',
+	    KeyConditionExpression=Key('date').eq(d)
+	)
+
+	c0 = 0
+	c1 = 0
+	if response.get('Items'):
+		for i in response['Items']:
+			if i.get('prefecture'):
+				i.pop('prefecture')
+			if i.get('place_no'):
+				i.pop('place_no')
+			if i.get('international_place_no'):
+				i.pop('international_place_no')
+			
+			print(i['date'], ":", i['snow_depth'])
+			if i['snow_depth']=='-':
+				i['snow_depth_val'] = decimal.Decimal("999.9")
+				c0 += 1
+			else:
+				i['snow_depth_val'] = decimal.Decimal(i['snow_depth'])
+				c1 += 1
+		
+		print('len(response["Items"] : {}'.format(len(response['Items'])))
+		print( 'c0: {}, c1: {}'.format(c0,c1) )
+
+		print('response :' + json.dumps(response, cls=DecimalEncoder))
+
+		with table.batch_writer() as batch:
+			for i in response['Items']:
+				batch.put_item(
+						Item=i
+				)
+
+	else:
+		print('あれれ。Itemsないみたい')
+
+
+# 降水量
 # append_rainfall_amount_val(2018,4,8)
 # append_rainfall_amount_val(2018,4,9)
 # append_rainfall_amount_val(2018,4,10)
@@ -123,5 +222,11 @@ def append_highest_temperature_val(year,month,day):
 # append_rainfall_amount_val(2018,4,15)
 # rain24h 完了
 
+# 最高気温
 # append_highest_temperature_val(2018,4,7)
-append_highest_temperature_val(2018,4,8)
+# append_highest_temperature_val(2018,4,8)
+# append_highest_temperature_val(2018,4,9)
+# append_highest_temperature_val(2018,4,10)
+# append_highest_temperature_val(2018,4,11)
+append_highest_temperature_val(2018,4,12)
+
