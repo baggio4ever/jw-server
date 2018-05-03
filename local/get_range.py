8b535597a061b19c9cf1b8b2dd55ab575cc9d78e
baggio4ever <baggio4ever@gmail.com>
2018-05-03 11:30:47 +0900

    最低気温他もを期間指定で取得できるした

--- local/get_range.py
+++ local/get_range.py
@@ -31,22 +31,72 @@ def get_highest_range(place,from_date,to_date):
     table = dynamo.Table(TABLE_NAME)
     
     print('-- 最高気温 range --')
-    print('place : ' + place)
-    print('from : ' + from_date)
-    print('to : ' + to_date)
+    print(place + '  ' + from_date + ' - ' + to_date)
             
     response = table.query(
         KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
         Limit=65
     )
             
-#            if len(response['Items']) == 0:
-#                logger.info('あれれ。該当データないみたい')
-
     for i in response['Items']:
         print( i['date'] + ' : '+ i['temperature'])
 
 
+def get_lowest_range(place,from_date,to_date):
+    TABLE_NAME = 'dev-jw-lowest'
+    table = dynamo.Table(TABLE_NAME)
+    
+    print('-- 最低気温 range --')
+    print(place + '  ' + from_date + ' - ' + to_date)
+            
+    response = table.query(
+        KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+        Limit=65
+    )
+            
+    for i in response['Items']:
+        print( i['date'] + ' : '+ i['temperature'])
 
 
-get_highest_range('今津','2018/04/15','2018/05/01')
+def get_rain24h_range(place,from_date,to_date):
+    TABLE_NAME = 'dev-jw-rain24h'
+    table = dynamo.Table(TABLE_NAME)
+    
+    print('-- 降水量 range --')
+    print(place + '  ' + from_date + ' - ' + to_date)
+            
+    response = table.query(
+        KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+        Limit=65
+    )
+            
+    for i in response['Items']:
+        print( i['date'] + ' : '+ i['rainfall_amount'])
+
+
+def get_snow_range(place,from_date,to_date):
+    TABLE_NAME = 'dev-jw-snow'
+    table = dynamo.Table(TABLE_NAME)
+    
+    print('-- 積雪量 range --')
+    print(place + '  ' + from_date + ' - ' + to_date)
+            
+    response = table.query(
+        KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+        Limit=65
+    )
+            
+    for i in response['Items']:
+        print( i['date'] + ' : '+ i['snow_depth'])
+
+
+
+
+get_highest_range('今津','2018/04/15','2018/05/02')
+print('')
+get_lowest_range('今津','2018/04/15','2018/05/02')
+print('')
+get_rain24h_range('今津','2018/04/15','2018/05/02')
+print('')
+get_snow_range('今津','2018/04/15','2018/05/02')
+print('')
