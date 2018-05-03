5fbf07c2beb37a82bdf5bbed0121d1d72e2e1e58
baggio4ever <baggio4ever@gmail.com>
2018-05-03 22:33:46 +0900

    ひとまず Web API 実装できた

--- handler3.py
+++ handler3.py
@@ -16,7 +16,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
  
 s3 = boto3.client('s3')
-BUCKET_NAME = os.environ['bucketName']
 
 dynamo = boto3.resource('dynamodb',region_name='ap-southeast-2')
 
@@ -28,41 +27,8 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
-# 観測ポイント名 の 開始日から終了日までのデータを取得する（最大65日）
-# from : 開始日
-# to  : 終了日
-# name : 観測ポイント名
 
-def get_highest_range(event, context):
-    TABLE_NAME = 'dev-jw-highest'
-    table = dynamo.Table(TABLE_NAME)
-
-    if( event.get('queryStringParameters')):
-        params = event.get('queryStringParameters')
-
-        name = params.get('name')
-        from_date = params.get('from')
-        to_date = params.get('to')
-        if name and from_date and to_date:
-            logger.info('-- 最高気温 range --')
-            logger.info('name : ' + name)
-            logger.info('from : ' + from_date)
-            logger.info('to : ' + to_date)
-            
-            response = table.query(
-                KeyConditionExpression=Key('place').eq(name) & Key('date').between(from_date,to_date),
-                Limit=65
-            )
-            
-            if len(response['Items']) == 0:
-                logger.info('あれれ。該当データないみたい')
-            
-            results = response['Items']
-        else:
-            results = []
-    else:
-        results = []
-        
+def getResponse(results):
     body = {
         "Items": results
     }
@@ -78,3 +44,146 @@ def get_highest_range(event, context):
     logger.info(response)
 
     return response
+
+
+DEFAULT_LIMIT = 65
+
+def query(event,table_name):
+    table = dynamo.Table(table_name)
+
+    if( event.get('queryStringParameters')):
+        params = event.get('queryStringParameters')
+
+        place = params.get('place')
+        from_date = params.get('from')
+        to_date = params.get('to')
+        if place and from_date and to_date:
+            logger.info( place + '  ' + from_date + ' - ' + to_date )
+            
+            response = table.query(
+                KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+                Limit=DEFAULT_LIMIT
+            )
+            
+            if len(response['Items']) == 0:
+                logger.info('あれれ。該当データないみたい')
+            
+            results = response['Items']
+        else:
+            results = []
+    else:
+        results = []
+
+    return results
+
+    
+# 観測ポイント名 の 開始日から終了日までのデータを取得する（最大65日）
+# from : 開始日
+# to  : 終了日
+# name : 観測ポイント名
+
+def get_highest_range(event, context):
+    TABLE_NAME = 'dev-jw-highest'
+#    table = dynamo.Table(TABLE_NAME)
+    results = query(event,TABLE_NAME)
+    '''
+    if( event.get('queryStringParameters')):
+        params = event.get('queryStringParameters')
+
+        place = params.get('place')
+        from_date = params.get('from')
+        to_date = params.get('to')
+        if place and from_date and to_date:
+            logger.info( place + '  ' + from_date + ' - ' + to_date )
+            
+            response = table.query(
+                KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+                Limit=DEFAULT_LIMIT
+            )
+            
+            if len(response['Items']) == 0:
+                logger.info('あれれ。該当データないみたい')
+            
+            results = response['Items']
+        else:
+            results = []
+    else:
+        results = []
+    '''    
+    return getResponse(results)
+#    body = {
+#        "Items": results
+#    }
+
+#    response = {
+#        "statusCode": 200,
+#        "body": json.dumps(body, cls=DecimalEncoder),
+#        "headers": {
+#            "Access-Control-Allow-Origin":"*"
+#        }
+#    }
+#    
+#    logger.info(response)
+
+#    return response
+
+
+def get_lowest_range(event, context):
+    TABLE_NAME = 'dev-jw-lowest'
+#    table = dynamo.Table(TABLE_NAME)
+    results = query(event,TABLE_NAME)
+    '''
+    if( event.get('queryStringParameters')):
+        params = event.get('queryStringParameters')
+
+        place = params.get('place')
+        from_date = params.get('from')
+        to_date = params.get('to')
+        if place and from_date and to_date:
+            logger.info( place + '  ' + from_date + ' - ' + to_date )
+            
+            response = table.query(
+                KeyConditionExpression=Key('place').eq(place) & Key('date').between(from_date,to_date),
+                Limit=DEFAULT_LIMIT
+            )
+            
+            if len(response['Items']) == 0:
+                logger.info('あれれ。該当データないみたい')
+            
+            results = response['Items']
+        else:
+            results = []
+    else:
+        results = []
+    '''        
+    return getResponse(results)
+#    body = {
+#        "Items": results
+#    }
+
+#    response = {
+#        "statusCode": 200,
+#        "body": json.dumps(body, cls=DecimalEncoder),
+#        "headers": {
+#            "Access-Control-Allow-Origin":"*"
+#        }
+#    }
+    
+#    logger.info(response)
+#
+#    return response
+
+def get_rain24h_range(event, context):
+    TABLE_NAME = 'dev-jw-rain24h'
+
+    results = query(event,TABLE_NAME)
+        
+    return getResponse(results)
+
+
+def get_snow_range(event, context):
+    TABLE_NAME = 'dev-jw-snow'
+
+    results = query(event,TABLE_NAME)
+        
+    return getResponse(results)
